89d223d921089db504cbdfd00b28b442c93ad78f
MarianoWalter <marianowalter.14@gmail.com>
2019-08-06 18:51:24 -0300

    Archivo de prueba y versión 1.0.3

---
+++ test.js
@@ -0,0 +1,59 @@
+// Test file
+
+/**
+ * Test function
+ * @returns { string }
+ */
+async function test() {
+	// Comment
+	return Promise.resolve(`Example ${1}`)
+		.then(str => `'${str}'`)
+		.catch(e => {
+			console.error('Error:', e);
+		});
+}
+
+let test_let = Promise.resolve(3);
+const test_const = (x => x)(test_var);
+
+var test_var = test_let ? 1 : {
+	attr1: 'first',
+	second: [
+		'a',
+		true,
+		{ test, b: 2.2 },
+		function() { /* anonymous */ },
+		async function test_attr1(_x) {
+			let c = 0;
+			while (c++ < 10) {
+				_x = await test_let;
+				if (c === 3) {
+					c += 2;
+				} else {
+					console['warn'].apply(console, [_x]);
+
+					/* eslint-disable-next-line no-debugger */
+					debugger;
+				}
+			}
+		},
+		function test_attr2(a, b, _c, _d) { // unused arguments
+			/* eslint-disable-next-line no-constant-condition */
+			while (true) {
+				/* eslint-disable-next-line no-useless-catch */
+				try {
+					if (a) {
+						const err = new Error();
+						throw err;
+					} else {
+						return test_const + a + b;
+					}
+				} catch (e) {
+					throw e; // no-useless-catch rule
+				}
+			}
+
+		},
+		async () => _test_return => test_var['second'] || false,
+	],
+};
